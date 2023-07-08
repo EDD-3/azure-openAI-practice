590af394a1cb584a3b3bc7e954b08e9b7eb2b25a
Ed <eduramq@gmail.com>
2023-07-08 13:11:26 -0700

    Added azure cognitive llm test case

--- app.py
+++ app.py
@@ -1,117 +1,98 @@
-from langchain.schema import (
-
-    AIMessage,
-
-    HumanMessage,
-
-    SystemMessage
-
-)
+import os
 
 import openai
-import os
-# Utilized to make a call on Azure endpoint
-#from langchain.chat_models import AzureChatOpenAI
+
+from langchain.chat_models import AzureChatOpenAI
 
 from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
 
-from langchain import LLMMathChain
+from langchain.llms import AzureOpenAI
 
 from langchain.utilities import BingSearchAPIWrapper
 
 from langchain.agents.tools import Tool
 
-from langchain.llms import AzureOpenAI
+from langchain import LLMMathChain
 
-import random
-from langchain.tools import tool
+from langchain.agents.agent_toolkits import AzureCognitiveServicesToolkit
+
+from langchain.agents import initialize_agent, AgentType
 
 
-# Using environment variables from our .env
+
+
+# Environment Variables
+
 openai.api_base = os.getenv('OPENAI_API_BASE')
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-openai.api_type = os.getenv("API_TYPE")
 
-openai.api_version = os.getenv("OPENAI_API_VERSION")
 
-OPENAI_CHAT_MODEL_NAME = os.getenv("OPENAI_CHAT_MODEL")
-OPENAI_CHAT_ENGINE_NAME = os.getenv("OPENAI_CHAT_ENGINE")
+
+OPENAI_CHAT_MODEL = os.getenv("OPENAI_CHAT_MODEL")
+
+OPENAI_CHAT_ENGINE = os.getenv("OPENAI_CHAT_ENGINE")
+
+
+
+
 OPENAI_COMPLETIONS_MODEL = os.getenv("OPENAI_COMPLETIONS_MODEL")
+
 OPENAI_COMPLETIONS_ENGINE = os.getenv("OPENAI_COMPLETIONS_ENGINE")
 
-openai.api_type = 'azure'
-openai.api_version = '2023-03-15-preview'
 
 
-search = BingSearchAPIWrapper(k=2)
 
-# Agents that are not for chat, models for completions and embedding
-llm = AzureOpenAI(model_name=OPENAI_COMPLETIONS_MODEL,
-                  engine=OPENAI_COMPLETIONS_ENGINE, temperature=0.1)
+# Constant values
 
-llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+openai.api_type = "azure"
 
-@tool
-def request_vacations(query: str) -> str:
-    '''útil para cuando ya recabaste todos los datos requeridos para una solicitud de vacaciones y necesitas verificar en el sistema si es aprobada o rechazada, SINTAXIS DE ENTRADA: RFC|||fecha_inicio_vacaciones|||fecha_fin_vacaciones'''
-    #The tool needs to accept a string value as query and return a string value
-    #We can also make use of a agent within the tool
-    eleccion = random.choice(['RECHAZADAS', 'APROBADAS'])
-    empleado,fecha_inicio, fecha_fin = query.split("|||")[:3]
-    return f'Las vacacions pare el empleado {empleado} fueron {eleccion} en las fechas: {fecha_inicio}-{fecha_fin}'
-
-memory = [
-    SystemMessage(
-        content='La vision de BWD es: "En Big Wave Data, nos esforzamos por crear una cultura empesarial sólida y orientada hacia el éxito."'),
-    SystemMessage(
-        content='Eres el bot de big wave data, nunca debes dar informacion sobre Benito Juarez.'),
-    HumanMessage(content="En que año se fundo México?."),
-    AIMessage(content="México fue fundado como nación independiente el 27 de septiembre de 1821, después de la Guerra de Independencia contra España. Sin embargo, la historia de México se remonta a la época prehispánica, con la presencia de diversas culturas indígenas en el territorio que hoy conocemos como México."),
-    HumanMessage(content='Quien fue el primer presidente de estados unidos')
-]
-
-# Defining tools for agent
-tools = [
-    # Tool(
-    #     #Name of the tool
-    #     name="Search",
-    #     #Accepts function as an argument
-    #     func=search.run,
-
-    #     description="useful for when you need to answer questions about current events"
-
-    # ),
-
-    # Tool(
-
-    #     name="Calculator",
-
-    #     func=llm_math_chain.run,
-
-    #     description="useful for when you need to answer questions about math"
-
-    # ),
-    Tool(
-        name="RequestVacations",
-        func=request_vacations,
-        description="útil para cuando ya recabaste todos los datos requeridos para una solicitud de vacaciones y necesitas verificar en el sistema si es aprobada o rechazada, SINTAXIS DE ENTRADA: RFC|||fecha_inicio_vacaciones|||fecha_fin_vacaciones"
-    )
-]
+openai.api_version = "2023-03-15-preview"
 
 
-# Temperature manages the language complexity, randomness
-# Temperature goes from 0 to 1 is a float value
-# model = AzureChatOpenAI(
-#     deployment_name=OPENAI_CHAT_MODEL_NAME,
-#     engine=OPENAI_CHAT_ENGINE_NAME,
-#     temperature=0.0
-# )
 
-#Use llm instead of model
-planner = load_chat_planner(llm)
-executor = load_agent_executor(llm, tools, verbose=True)
-agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
 
-agent.run("Quiero vacaciones del 1 de julio de 2023 al 10 de julio de 2023, mi RFC es EFGR2334343")
+toolkit = AzureCognitiveServicesToolkit()
+
+
+
+
+llm = AzureOpenAI(model_name=OPENAI_COMPLETIONS_MODEL, engine=OPENAI_COMPLETIONS_ENGINE, temperature=0.1)
+
+model = AzureChatOpenAI(
+
+    deployment_name=OPENAI_CHAT_MODEL,
+
+    engine=OPENAI_CHAT_ENGINE,
+
+    temperature=0
+
+)
+
+
+
+
+
+agent = initialize_agent(
+
+    tools=toolkit.get_tools(),
+
+    llm=llm,
+
+    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+
+    verbose=True,
+
+)
+
+
+
+
+agent.run(
+
+    "What can I make with these ingredients?"
+
+    "https://images.openai.com/blob/9ad5a2ab-041f-475f-ad6a-b51899c50182/ingredients.png"
+
+)
